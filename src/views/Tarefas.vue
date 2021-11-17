91c15b1e6caf496bb7c9cf124b15cba78ef8b898
Erickolk <erickdynsouza@hotmail.com>
2021-11-17 17:24:51 -0300

    Iniciando Css

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <h1>Minha Primeira Tabela</h1>
+    <table border="1" width="100%" bordercolor ="blue">
+      <th>Status</th>
+      <th>Descricão</th>
+      <th>Tempo de realização</th>
+      <th>Área de botões</th>
+
+      <tr>
+        <td>Status</td>
+        <td>Descricão</td>
+        <td>Tempo de realização</td>
+        <td>Área para botões</td>
+      </tr>
+      <tr>
+        <td>Realizado</td>
+        <td>Tempo regular</td>
+        <td>5 minutos</td>
+        <td>BOTTON</td>
+      </tr>      
+      <tr>
+        <td>Á realizar</td>
+        <td>Regular tabela</td>
+        <td>Indeterminado</td>
+        <td></td>
+      </tr>
+            <tr>
+        <td>Realizado</td>
+        <td>Tempo regular</td>
+        <td>5 minutos</td>
+        <td></td>
+      </tr>
+
+    </table>
+  </div>
+<!--HTML5-->
+</template>
+<style>
+/*Aqui fica o CSS*/
+
+table th {
+  color:red;
+}
+</style>
