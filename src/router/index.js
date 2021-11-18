5365fb7e8473da18763f77f9a05e957e00e9e5f6
Erickolk <erickdynsouza@hotmail.com>
2021-11-18 14:00:40 -0300

    Criei a rota de tarefas e formulários

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Tarefas from '../views/Tarefas.vue'
+import TarefasFormulario from '../views/TarefasFormulario.vue'
 
 
 Vue.use(VueRouter)
@@ -13,12 +14,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/tarefas-formulario',
+    name: 'TarefasFormulario',
+    component: TarefasFormulario
+    
   },
   {
     path: '/tarefas',
